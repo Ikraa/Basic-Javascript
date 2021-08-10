0ea07476369175221cffba21c18df8d8cd6632c8
Ikraa <munnisaikra750@gmail.com>
2021-08-11 02:16:27 +0600

    fibonaccy

--- recursion-loop.js
+++ recursion-loop.js
@@ -31,7 +31,7 @@ function sum(i){
 console.log(sum(6));
 
 
-// // increment recursion by using function with if loop:
+// increment recursion by using function with if loop:
 
 function sum(i){
     if(i==6){
@@ -44,8 +44,7 @@ console.log(sum(1));
 
 
 
-
-
+// ------------------------------------------------------------------------------
 // Example of Recursive Factorial:
 // 1!=1                                                     1!=1                     
 // 2!=2*1                                                   2!=2
@@ -67,14 +66,57 @@ console.log(factorial);
 
 
 
-//// recursion factorial using by function
+// recursion factorial using by function(decreament):
 
 function factorial(i){
 
-    if (i == 1) {
+    if (i == 1) {    // stopping condition
     
         return 1;
     }
         return i*factorial(i-1);
     }
     console.log(factorial(6));
+
+
+
+
+// Example of Fibonaccy:--------------------------------------
+//  0 1 1 2 3 5 8 13 21 34 55
+
+//  1st=1st+0;
+//  2nd=1st+1st
+//  3rd=2nd+1st
+//  4th=3rd+2nd
+//  5th=4th+3rd
+//  6th=5th+4th
+//  119th=118th+117th
+//  nth=(n-1)th+(n-2)th
+
+
+// Fibonaccy Recursion using by for loop:
+
+let fibo=[0, 1];
+ for(i=2; i<=6; i++)
+ {
+     fibo[i]=fibo[i-1] + fibo[i-2];
+    
+ }
+ console.log(fibo);
+
+
+ 
+ //Fibonaccy Recursion using by function:
+     
+function fibo(i){
+    if(i ==0){
+
+        return 0;
+    }                   //stoping condtion
+    if(i ==1 ){
+        
+        return 1;
+    }
+    return fibo(i - 1) + fibo(i - 2);
+}
+console.log(fibo(6));
